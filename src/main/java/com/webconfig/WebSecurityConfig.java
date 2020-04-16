30aa185d6353b5aece27c4a6daf8fab283393539
ricofung <ricofungcn@icloud.com>
2020-04-16 16:55:35 +0800

    整合为oauth2-resourse模式

--- src/main/java/com/webconfig/WebSecurityConfig.java
+++ src/main/java/com/webconfig/WebSecurityConfig.java
@@ -1,54 +1,56 @@
 package com.webconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//import com.epo.service.TbBiHelpUserService;
-
-@EnableWebSecurity
+@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
 	////////////////////////////////////////////////////////////
+	// Oauth2 认证
+	////////////////////////////////////////////////////////////
+	@Override
+	public void configure(HttpSecurity http) throws Exception
+	{
+		http.csrf().disable()
+        ;
+	}
+	
+	////////////////////////////////////////////////////////////
 	// HttpBasic 认证
 	////////////////////////////////////////////////////////////
-	@Autowired
-	private Environment env;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception
-	{
-		http.authorizeRequests().antMatchers("/**").authenticated().and().httpBasic();
-		http.csrf().disable();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService()
-	{
-		// Get the user credentials from the console (or any other source):
-		String username = env.getProperty("spring.security.user.name");
-		String password = env.getProperty("spring.security.user.password");
-
-		// Set the inMemoryAuthentication object with the given credentials:
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		String encodedPassword = passwordEncoder().encode(password);
-		manager.createUser(User.withUsername(username).password(encodedPassword).roles("ADMIN").build());
-		return manager;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
-		return new BCryptPasswordEncoder();
-	}
+//	@Autowired
+//	private Environment env;
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception
+//	{
+//		http.authorizeRequests().antMatchers("/**").authenticated().and().httpBasic();
+//		http.csrf().disable();
+//	}
+//
+//	@Bean
+//	public UserDetailsService userDetailsService()
+//	{
+//		// Get the user credentials from the console (or any other source):
+//		String username = env.getProperty("spring.security.user.name");
+//		String password = env.getProperty("spring.security.user.password");
+//
+//		// Set the inMemoryAuthentication object with the given credentials:
+//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//		String encodedPassword = passwordEncoder().encode(password);
+//		manager.createUser(User.withUsername(username).password(encodedPassword).roles("ADMIN").build());
+//		return manager;
+//	}
+//
+//	@Bean
+//	public PasswordEncoder passwordEncoder()
+//	{
+//		return new BCryptPasswordEncoder();
+//	}
+	
+	
 	
 	////////////////////////////////////////////////////////////
 	// Http 表单认证
